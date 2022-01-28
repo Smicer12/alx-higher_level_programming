405034ff242cf144340f9bb95eb1c26ddc431ccd
smicer12 <developer.smicer12@gmail.com>
2022-01-28 21:41:42 +0300

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(0, 10):
-    for n in range(0, 10):
-        print("{}{}, ".format(i, n), end='')
-print("\n")
+for i in range(0, 100):
+    if i < 10:
+        print("0{}, ".format(i), end='')
+    else:
+        print("{}, ".format(i), end='')
