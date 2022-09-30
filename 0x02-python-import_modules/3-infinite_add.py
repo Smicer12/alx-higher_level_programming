8a5b0be0922c9f237875daca0a83a23eddb7456f
smicer12 <developer.smicer@gmail.com>
2022-09-30 04:56:58 -0700

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,13 @@
 import sys
 if __name__ == "main":
     pass
-a = sys.argv[1:]
-b = []
-for i in a:
-    num = int(i)
-    b.append(num)
-print (sum(b) )
+argv = sys.argv[1:]
+count = len(argv)
+index = 1
+result = 0
+
+while index <= count:
+    result += int(sys.argv[index])
+    index += 1
+
+print("{:d}".format(result))
