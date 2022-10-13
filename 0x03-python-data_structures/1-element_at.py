ffa370e4dce02d3d1f24e6bd271acbaad49dbfda
smicer12 <developer.smicer12@gmail.com>
2022-10-13 13:54:22 +0300

    task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx > len(my_list):
+        return None
+    else:
+        print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
+
+
+element_at()
