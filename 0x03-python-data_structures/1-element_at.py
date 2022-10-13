692289ea0286fdf03ff6d7332e4a7aa4bf96f4ea
smicer12 <developer.smicer12@gmail.com>
2022-10-13 15:11:33 +0300

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-from main_1 import my_list, idx
-from main_2 import my_list, idx
 def element_at(my_list, idx):
     if idx < 0:
         print("Element at index {:d} is None".format(idx))
@@ -8,6 +6,3 @@ def element_at(my_list, idx):
         print("Element at index {:d} is None".format(idx ))
     else:
         print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
-
-
-element_at(my_list, idx)
